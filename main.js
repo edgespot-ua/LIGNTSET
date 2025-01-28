2657f8dab84022e015a047627473714e04ff405b
Gz1st <dmitro1208@gmail.com>
2025-01-28 22:55:54 +0200

    if  switch

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ console.log("qq");
 else{
     console.log("erorr!");
 }
+
+
+/* --------------------------------------*/
+
+
+var stroka = "word";
+
+switch(stroka){
+
+case "4": console.log("ППеременная со значениям 4");
+break;
+
+
+case "56": console.log("ППеременная со значениям 56");
+break;
+
+case "word": console.log("ППеременная со значениям word");
+break;
+
+default: console.log("default");
+break;
+}
