dce8612f4f694623001f74ef7e76869e1855c4c5
Gz1st <dmitro1208@gmail.com>
2025-01-28 22:22:59 +0200

    таптап

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 
-var num = 15 ;
 
 
-if(num == 15 ){
+
+
+/* -----УМОВНІ ОПЕРАТОРИ----*/
+var num_les5 = 15 ;
+
+
+if(num_les5 != 15 ){
 console.log("ok");
+}
+
+else if(num_les5<10){
+console.log("qq");
+}
+
+else{
+    console.log("erorr!");
 }
