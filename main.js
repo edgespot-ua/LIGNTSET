5662722724d56d0803a044b464796951cb2c2919
Gz1st <Dmitro12082004@gmail.com>
2025-02-20 18:17:05 +0200

    func

--- main.js
+++ main.js
@@ -40,3 +40,54 @@ break;
 default: console.log("default");
 break;
 }
+
+/*------вспливаючі вінка ---*/
+/*alert()
+prompt()
+confirm()*/
+
+
+
+
+
+/*-----функції---------------------*/
+
+function info(word){
+    console.log(word +"!");
+   
+}
+info("hello");
+
+
+
+
+function summa(a, b){
+var res = a + b;
+console.log(res);
+}
+summa(2, 4);
+
+   
+
+
+
+function sumarr(arr){
+
+    var sum = 0;
+
+  for(var i = 0; i < arr.length; i++ ){
+
+  sum+= array[i];
+  }
+
+  console.log(sum);
+}
+
+var array = [6,4,2];
+
+sumarr(array);
+
+
+
+
+
