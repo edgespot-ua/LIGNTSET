1ba65ea65b107e434c455a2a262ea2bbfa853b8b
edgespot <dmitro1208@gmail.com>
2025-03-20 12:57:43 +0200

    gerggg

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const sql = require('mssql');
 
-const config = {
-    server: 'DESKTOP-UVSPOF4',
-    database: 'data_1',
-    options: { encrypt: true, trustServerCertificate: true },
-    authentication: {
-        type: 'ntlm',
-        options: {
-            domain: 'DESKTOP-UVSPOF4'
-        }
-    }
-};
-
-async function getProducts() {
-    try {
-        await sql.connect(config);
-        const result = await sql.query('SELECT * FROM products');
-        console.log(result.recordset);
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-getProducts();
+//виведденя інофрмації в консоль
+console.log('привет');
